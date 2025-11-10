cab0c385ff66754f7f2fd6172e98cd8968c81939
NyasiaMcCoy09 <mccoy.nyasia09@gmail.com>
2025-11-10 17:34:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,3 @@
         When the script is run directly (not imported as a module), it will execute the main() function.
 """
 
-
-def get_user_name():
-    return input("Please enter your name: ")
-
-def greet_user(name):
-    print(f"Hello, {name}!")
-
-def main():
-    user_name = get_user_name()
-    greet_user(user_name)
-
-if __name__ == "__main__":
-    main()
